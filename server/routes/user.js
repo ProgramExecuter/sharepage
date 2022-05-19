a6703d349f4e598678b502cae6ae030e816e534f
Amit Singh Rana <ranaa73000@gmail.com>
2022-05-19 12:27:35 +0530

    Adding skeleton for USER Routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,4 +2,19 @@ import express from "express";
 
 const router = express.Router();
 
+// GET A USER
+router.get("/:id", (req, res) => {
+  res.send(`GET /user/${req.params.id}`);
+});
+
+// EDIT A USER
+router.patch("/:id", (req, res) => {
+  res.send(`PATCH /user/${req.params.id}`);
+});
+
+// DELETE A USER
+router.delete("/:id", (req, res) => {
+  res.send(`DELETE /user/${req.params.id}`);
+});
+
 export default router;
