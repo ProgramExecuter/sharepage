be962385f05b7ec86c796515c6c317dec7574667
Amit Singh Rana <ranaa73000@gmail.com>
2022-05-19 12:23:32 +0530

    Adding skeleton for AUTH Routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,4 +2,19 @@ import express from "express";
 
 const router = express.Router();
 
+// LOGIN
+router.post("/login", (req, res) => {
+  res.send("POST /auth/login");
+});
+
+// SIGNUP(Register new User)
+router.post("/signup", (req, res) => {
+  res.send("POST /auth/signup");
+});
+
+// LOGOUT
+router.post("/logout", (req, res) => {
+  res.send("POST /auth/logout");
+});
+
 export default router;
