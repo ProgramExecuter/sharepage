53db301162bd1185ba107ec3045d34e3978ccaff
Amit Singh Rana <ranaa73000@gmail.com>
2022-05-19 12:34:07 +0530

    Adding skeleton for COMMENT Routes

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -2,4 +2,19 @@ import express from "express";
 
 const router = express.Router();
 
+// CREATE A COMMENT
+router.post("/", (req, res) => {
+  res.send("POST /comment");
+});
+
+// EDIT A COMMENT
+router.patch("/:id", (req, res) => {
+  res.send(`PATCH /comment/${req.params.id}`);
+});
+
+// DELETE A COMMENT
+router.delete("/:id", (req, res) => {
+  res.send(`DELETE /comment/${req.params.id}`);
+});
+
 export default router;
