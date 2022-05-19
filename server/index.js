e914a2492640c61b2a52471c121c18e97d83dbad
Amit Singh Rana <ranaa73000@gmail.com>
2022-05-19 12:08:48 +0530

    Added routing(routes)

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoute from "./routes/auth.js";
+import postRoute from "./routes/post.js";
+import userRoute from "./routes/user.js";
+import commentRoute from "./routes/comment.js";
+
 //////////////////////
 ///   ENV Setup   ///
 ////////////////////
 dotenv.config();
 
 const app = express();
+
+////////////////////
+///   ROUTES   ////
+//////////////////
+app.use("/auth", authRoute);
+app.use("/post", postRoute);
+app.use("/user", userRoute);
+app.use("/comment", commentRoute);
 
 ////////////////////////
 ///  Server Setup   ///
