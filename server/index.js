ee46090865fc331ed6ebec6bd1cc5573458b5624
Amit Singh Rana <ranaa73000@gmail.com>
2022-05-19 11:05:00 +0530

    refactoring

--- server/index.js
+++ server/index.js
@@ -12,10 +12,6 @@ const app = express();
 ///  Server Setup   ///
 //////////////////////
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Server Listening at PORT ${PORT}`);
-  }
+app.listen(PORT, () => {
+  console.log(`Server Listening at PORT ${PORT}`);
 });
