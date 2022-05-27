d6922186c6e3c53ebafac9b4dcf06c983725b3e8
Amit Singh Rana <ranaa73000@gmail.com>
2022-05-27 19:54:54 +0530

    Connected to DB

--- server/index.js
+++ server/index.js
@@ -5,9 +5,26 @@ import authRoute from "./routes/auth.js";
 import postRoute from "./routes/post.js";
 import userRoute from "./routes/user.js";
 import commentRoute from "./routes/comment.js";
+import mongoose from "mongoose";
 
 // ENV Setup
 dotenv.config();
+
+// MongoDB connection
+mongoose.connect(
+  process.env.MONGOURI,
+  {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("DB Connected");
+    }
+  }
+);
 
 const app = express();
 
